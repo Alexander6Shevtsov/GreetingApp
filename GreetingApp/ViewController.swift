03b588afb034c6d7677a536a6c0509bcc755e20c
Alexander6Shevtsov <axrsvr@gmail.com>
2025-05-10 19:51:18 +0300

    Initial Commit

---
+++ GreetingApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GreetingApp
+//
+//  Created by Alexander Shevtsov on 10.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var greetingLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+    }
+}
+
