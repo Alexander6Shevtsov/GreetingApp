a984024f02cf9f74d316e0e27e5cd5f1a0e9db02
Alexander6Shevtsov <axrsvr@gmail.com>
2025-05-11 21:21:22 +0300

    MVC Example

---
+++ GreetingApp/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  GreetingApp
+//
+//  Created by Alexander Shevtsov on 10.05.2025.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+}
