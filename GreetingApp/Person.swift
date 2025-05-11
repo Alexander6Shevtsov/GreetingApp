1596c01e58586d7fad602492a954883cc0c64c5b
Alexander6Shevtsov <axrsvr@gmail.com>
2025-05-11 21:48:02 +0300

    Add Person

---
+++ GreetingApp/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  GreetingApp
+//
+//  Created by Alexander Shevtsov on 11.05.2025.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+}
